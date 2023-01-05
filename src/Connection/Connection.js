0de37522dcc096a592738226b2b0696d611ea31a
raycoga <capram94@gmail.com>
2023-01-05 13:19:33 -0400

    cambios en la url de conexion

--- src/Connection/Connection.js
+++ src/Connection/Connection.js
@@ -1,6 +1,30 @@
-const env = 'dev';
+// const env = 'qa';
+const env = "dev";
+// const env = 'prod';
 
 const prod = {
+  REACT_DOMAIN_BACK: "https://newtechapp.net/api/request/bnp",
+};
+const dev = {
+  REACT_DOMAIN_BACK: "https://test.developerpa.com/api/request/bnp",
+};
+const qa = {
+  REACT_DOMAIN_BACK: "https://app.developerpa.com/api/request/bnp",
+};
+
+let api = "";
+if (env === "prod") api = prod;
+if (env === "dev") api = dev;
+if (env === "qa") api = qa;
+
+module.exports = { api };
+
+//https://newtechapp.net/
+// servidor para QA https://app.developerpa.com
+
+// /BEN/selfie?requestId=e729cc41-500e-41a2-8750-53b445801a94&returnUrl=https://onboardingbnp.mpaycenter.com/onboarding/verification&phone=61503002&email=capram94@gmail.com
+
+/* const prod = {
   domainServer: 'https://newtechapp.net',
   domainPython: 'https://newtechapp.net',
   bucket: 'buckface',
@@ -12,15 +36,6 @@ const dev = {
   bucket: 'myawsbucketface',
   mpay: 'https://walletws.banconal.com.pa:8443/bnpswitch2v3/onboarding/verification',
   REACT_DOMAIN_BACK: 'https://app3.developerpa.com/request/bnp'
-};
+}; */
 
-// servidor para QA https://app.developerpa.com
-const api = env === 'prod' ? prod : dev;
-
-module.exports = { api };
-
-
-//https://newtechapp.net/
-
-
-// /BEN/selfie?requestId=e729cc41-500e-41a2-8750-53b445801a94&returnUrl=https://onboardingbnp.mpaycenter.com/onboarding/verification&phone=61503002&email=capram94@gmail.com
+/* const api = env === 'prod' ? prod : dev; */
