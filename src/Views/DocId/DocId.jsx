d13c0303b36e4122d8ce8c78baa21883f4c5dd70
raycoga <capram94@gmail.com>
2023-01-19 21:23:52 -0400

    ultimos cambios

--- src/Views/DocId/DocId.jsx
+++ src/Views/DocId/DocId.jsx
@@ -27,29 +27,39 @@ const DocID = () => {
   const photoRef = useRef(null);
 
   const getVideo = (videoConstraints) => {
-    console.log(videoRef);
+    const { facingMode } = videoConstraints;
+    const CONSTRAINTS = {
+      audio: false,
+      video: {
+        width: {
+          min: 320,
+        },
+        height: {
+          min: 180,
+        },
+
+        /* width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440,
+    }, */
+        /* width: { min: 320, ideal: 640, max: 1280 },
+        height: { min: 180, ideal: 360, max: 720 }, */
+        facingMode,
+        frameRate: { min: 30 },
+      },
+    };
     if (
       "mediaDevices" in navigator &&
       "getUserMedia" in navigator.mediaDevices
     ) {
-      const { facingMode } = videoConstraints;
       navigator.mediaDevices
-        .getUserMedia({
-          video: {
-            width: {
-              min: 1280,
-              ideal: 1920,
-              max: 2560,
-            },
-            height: {
-              min: 720,
-              ideal: 1080,
-              max: 1440,
-            },
-            facingMode,
-            frameRate: { min: 30, ideal: 60 },
-          },
-        })
+        .getUserMedia(CONSTRAINTS)
         .then((stream) => {
           let video = videoRef.current;
           if ("srcObject" in video) {
@@ -58,7 +68,10 @@ const DocID = () => {
             // Avoid using this in new browsers
             video.src = window.URL.createObjectURL(stream);
           }
-          video.autofocus = true;
+          /*  video.autofocus = true; */
+          video.setAttribute("autoplay", "");
+          video.setAttribute("muted", "");
+          video.setAttribute("playsinline", "");
           /*  console.log(video); */
           video.play();
         })
@@ -67,12 +80,14 @@ const DocID = () => {
         });
     }
   };
-
   const takePhoto = () => {
-    const width = 1440;
-    const height = width / (16 / 9);
     let video = videoRef.current;
     let photo = photoRef.current;
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    /* const width = 1024;
+    const height = 1024;
+     const height = width / (16 / 9); */
     photo.width = width;
     photo.height = height;
     const canvas = document.createElement("canvas");
@@ -80,7 +95,8 @@ const DocID = () => {
     canvas.width = width;
     canvas.height = height;
     ctxCanvas.drawImage(video, 0, 0, width, height);
-    const imageBase64 = canvas.toDataURL("image/webp", 1);
+    const imageBase64 = canvas.toDataURL("image/png", 1);
+    /* navigator.clipboard.writeText(imageBase64); */
     dispatch(
       setPreviewDoc({ imagePrev: imageBase64, image: imageBase64, file: "" })
     );
@@ -104,13 +120,8 @@ const DocID = () => {
         progressCount={2}
       >
         <div className="camMask"></div>
-        <div className="camera2">
-          <video
-            autoPlay={true}
-            playsInline={true}
-            muted={true}
-            ref={videoRef}
-          ></video>
+        <div id="camera-v2.3" className="video-container">
+          <video ref={videoRef}></video>
         </div>
 
         <div style={{ position: "absolute", zIndex: "-12" }}>
