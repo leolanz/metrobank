cda22d64e4a06d8c3dac00353eb717646ac27463
leolanz <llanz@newtech.net>
2023-04-18 13:43:12 -0400

    Cambio de bucket y api

--- src/Connection/Connection.js
+++ src/Connection/Connection.js
@@ -3,17 +3,17 @@ const env = 'dev';
 const prod = {
   domainServer: 'https://newtechapp.net',
   domainPython: 'https://newtechapp.net',
-  bucket: 'buckface',
+  bucket: 'onboarding-repo',
   mpay: 'https://walletws.banconal.com.pa:8443/bnpswitch2v3/onboarding/verification',
 };
 const dev = {
   domainServer: 'https://app.developerpa.com', // https://app.developerpa.co estos dos dominios apuntan a la misma BD
   domainPython: 'https://app.developerpa.com',
-  bucket: 'myawsbucketface',
+  bucket: 'onboarding-repo',
   mpay: 'https://onboardingbnp.mpaycenter.com/onboarding/verification',
 };
 
 // servidor para QA https://app.developerpa.com
-const api = env === 'prod' ? prod : dev;
+const api = env === 'dev' ? dev : dev;
 
 module.exports = { api };
